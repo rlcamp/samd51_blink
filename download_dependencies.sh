d4455b7ac7ad05a0589c570d5104b65e299299de
Richard Campbell <rlcamp@users.noreply.github.com>
2023-09-24 20:49:56 -0700

    very belated commit of dependency script updates

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# this script works on macos monterey, as of 20220816, provided xz has been compiled from source
+# this script works on arm64 macos ventura, as of 20230223, provided xz has been compiled from source
 
 cd ~/Downloads/
 
@@ -11,11 +11,18 @@ curl -O http://packs.download.atmel.com/Atmel.SAMD51_DFP.1.2.139.atpack
 unzip Atmel.SAMD51_DFP.1.2.139.atpack
 cd ..
 
-curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi.tar.xz
-tar Jxf arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi.tar.xz
-cd arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi
-find . -perm +111 -type f | while read k; do cat $k > .tmp && chmod +x .tmp && mv .tmp $k; done
-cd ..
+if [ ! -d ~/Downloads/arm-gnu-toolchain-*-arm-none-eabi ]; then
+    if uname -sm | grep 'Linux x86_64' ; then
+        curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-rel1/binrel/arm-gnu-toolchain-12.2.mpacbti-rel1-x86_64-arm-none-eabi.tar.xz
+        tar Jxf arm-gnu-toolchain-12.2.mpacbti-rel1-x86_64-arm-none-eabi.tar.xz
+    else
+        curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-rel1/binrel/arm-gnu-toolchain-12.2.mpacbti-rel1-darwin-arm64-arm-none-eabi.tar.xz
+        tar Jxf arm-gnu-toolchain-12.2.mpacbti-rel1-darwin-arm64-arm-none-eabi.tar.xz
+        cd arm-gnu-toolchain-12.2.mpacbti-rel1-darwin-arm64-arm-none-eabi
+        find . -perm +111 -type f | while read k; do cat $k > .tmp && chmod +x .tmp && mv .tmp $k; done
+        cd ..
+    fi
+fi
 
 git clone --depth 1 https://github.com/shumatech/BOSSA
 cd BOSSA
