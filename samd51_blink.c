b69e60940f66b337778bbcdf1757c6fff1667cc3
Richard Campbell <rlcamp@users.noreply.github.com>
2023-10-16 11:58:06 -0700

    make sure tc3 works in standby

--- samd51_blink.c
+++ samd51_blink.c
@@ -25,6 +25,15 @@ void led_on(void) { PORT->Group[0].OUTSET.reg = 1U << PORT0_LED_PIN; }
 void led_off(void) { PORT->Group[0].OUTCLR.reg = 1U << PORT0_LED_PIN; }
 
 void timer_init(void) {
+    /* assume GCLK3 is one or the other 32 kHz reference, and we need to make sure it is
+     enabled and allowed to run in stdby */
+#ifdef CRYSTALLESS
+    OSC32KCTRL->OSCULP32K.bit.EN32K = 1;
+#else
+    OSC32KCTRL->XOSC32K.reg |= OSC32KCTRL_XOSC32K_EN32K | OSC32KCTRL_XOSC32K_RUNSTDBY;
+    while (!OSC32KCTRL->STATUS.bit.XOSC32KRDY);
+#endif
+
     /* make sure the APB is enabled for TC3 */
     MCLK->APBBMASK.reg |= MCLK_APBBMASK_TC3;
     
@@ -42,6 +51,9 @@ void timer_init(void) {
     /* timer ticks will be 32 kHz clock ticks divided by this prescaler value */
     TC3->COUNT16.CTRLA.bit.PRESCALER = TC_CTRLA_PRESCALER_DIV1_Val;
     
+    /* run in standby */
+    TC3->COUNT16.CTRLA.bit.RUNSTDBY = 1;
+
     /* counter resets after the value in cc[0], i.e. its period is that number plus one */
     TC3->COUNT16.WAVE.reg = TC_WAVE_WAVEGEN_MFRQ;
     TC3->COUNT16.CC[0].reg = 16383;
