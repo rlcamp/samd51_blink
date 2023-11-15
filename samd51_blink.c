f756a7ad504cc0a6bc639835c3285b5eaa789799
Richard Campbell <rlcamp@users.noreply.github.com>
2023-11-14 23:10:16 -0800

    overhaul timer register config, use wfe, standby, superloop pattern

--- samd51_blink.c
+++ samd51_blink.c
@@ -1,11 +1,12 @@
 #if __has_include(<samd51.h>)
-/* newer cmsis-atmel from upstream */
+/* as invoked by Makefile, regardless of cmsis-atmel version */
 #include <samd51.h>
 #else
-/* older cmsis-atmel from adafruit */
-#include <samd.h>
+/* as invoked by arduino ide */
+#include <samd51/include/samd51.h>
 #endif
 
+/* assume itsybitsy m4 pinout if this is defined */
 #ifdef CRYSTALLESS
 #define PORT0_LED_PIN 22U
 #else
@@ -28,15 +29,19 @@ void timer_init(void) {
 #ifdef CRYSTALLESS
     OSC32KCTRL->OSCULP32K.bit.EN32K = 1;
 #else
-    OSC32KCTRL->XOSC32K.reg |= OSC32KCTRL_XOSC32K_EN32K | OSC32KCTRL_XOSC32K_RUNSTDBY;
+    OSC32KCTRL->XOSC32K.bit.EN32K = 1;
+    OSC32KCTRL->XOSC32K.bit.RUNSTDBY = 1;
     while (!OSC32KCTRL->STATUS.bit.XOSC32KRDY);
 #endif
 
     /* make sure the APB is enabled for TC3 */
-    MCLK->APBBMASK.reg |= MCLK_APBBMASK_TC3;
-    
+    MCLK->APBBMASK.bit.TC3_ = 1;
+
     /* use the 32 kHz clock peripheral as the source for TC3 */
-    GCLK->PCHCTRL[TC3_GCLK_ID].reg = GCLK_PCHCTRL_GEN_GCLK3_Val | (1U << GCLK_PCHCTRL_CHEN_Pos);
+    GCLK->PCHCTRL[TC3_GCLK_ID].reg = (GCLK_PCHCTRL_Type) { .bit = {
+        .GEN = GCLK_PCHCTRL_GEN_GCLK3_Val,
+        .CHEN = 1
+    }}.reg;
     while (GCLK->SYNCBUSY.reg);
     
     /* reset the TC3 peripheral */
@@ -50,42 +55,54 @@ void timer_init(void) {
     }}.reg;
 
     /* counter resets after the value in cc[0], i.e. its period is that number plus one */
-    TC3->COUNT16.WAVE.reg = TC_WAVE_WAVEGEN_MFRQ;
+    TC3->COUNT16.WAVE.reg = (TC_WAVE_Type) { .bit.WAVEGEN = TC_WAVE_WAVEGEN_MFRQ_Val }.reg;
     TC3->COUNT16.CC[0].reg = 16383;
-    
-    /* fire an interrupt whenever counter equals that value */
+    while (TC3->COUNT16.SYNCBUSY.bit.COUNT);
+
+    /* fire an interrupt at lowest priority whenever counter equals that value */
     TC3->COUNT16.INTENSET.bit.MC0 = 1;
     NVIC_EnableIRQ(TC3_IRQn);
-    
+
     /* enable the timer */
     while (TC3->COUNT16.SYNCBUSY.reg);
     TC3->COUNT16.CTRLA.bit.ENABLE = 1;
     while (TC3->COUNT16.SYNCBUSY.bit.ENABLE);
 }
 
-static unsigned long wakes = 0;
+static unsigned wakes = 0;
 
 void TC3_Handler(void) {
-    if (!TC3->COUNT16.INTFLAG.bit.MC0) return;
+    /* clear flag so that interrupt doesn't re-fire */
     TC3->COUNT16.INTFLAG.reg = (TC_INTFLAG_Type){ .bit.MC0 = 1 }.reg;
-    
+
     wakes++;
 }
 
-void sleep_until_next_timer_interval(void) {
-    static unsigned long wakes_acknowledged = 0;
-    while (*(volatile unsigned long *)&wakes == wakes_acknowledged) __WFI();
-    wakes_acknowledged++;
-}
-
 int main(void) {
+    PM->SLEEPCFG.bit.SLEEPMODE = PM_SLEEPCFG_SLEEPMODE_STANDBY_Val;
+
     led_init();
     timer_init();
-    
+
+    unsigned wakes_acknowledged = 0;
+
     while (1) {
-        sleep_until_next_timer_interval();
-        led_on();
-        sleep_until_next_timer_interval();
-        led_off();
+        if (*(volatile unsigned *)&wakes != wakes_acknowledged) {
+            wakes_acknowledged++;
+
+            /* toggle the led */
+            if (wakes_acknowledged % 2) led_on();
+            else led_off();
+        }
+
+        /* sleep until next event (in this case the tc3 interrupt), unless the event register
+         is already set, in which case it is cleared and no sleep occurs. note since no other
+         code calls WFE, there will be at least two passes through the main loop for every
+         interrupt, where on the final pass, no state is changed, ER is already cleared, and
+         the processor sleeps. in exchange for these redundant passes before sleeping, we get
+         a guarantee that we will never spuriously sleep when we should have stayed awake to
+         handle an already-pending interrupt, as is possible with WFI if not disabling
+         interrupts during sleep (which adds latency in handling them on wake) */
+        __WFE();
     }
 }
