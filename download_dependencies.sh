4f55b008e5209ed732fbe7546752cab148215381
Richard Campbell <rlcamp@users.noreply.github.com>
2025-01-10 23:00:03 -0800

    update dependencies script

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -1,36 +1,31 @@
 #!/bin/sh
-# this script works on arm64 macos sonoma, as of 20240706, provided xz has been compiled from source
+# this script tested working on arm64 macos sequoia provided xz has been compiled from source
+set -eu
 
+# the Makefile is going to look for each of these things in ~/Downloads/ unless told otherwise
 cd ~/Downloads/
-rm -rf BOSSA
-rm -rf CMSIS_5
 
-git clone --depth 1 https://github.com/ARM-software/CMSIS_5
+if [ ! -d CMSIS_5 ]; then git clone --depth 1 https://github.com/ARM-software/CMSIS_5 ; fi
 
-mkdir -p Atmel.SAMD51_DFP.1.2.139
-cd Atmel.SAMD51_DFP.1.2.139
-curl -O http://packs.download.atmel.com/Atmel.SAMD51_DFP.1.2.139.atpack
-unzip Atmel.SAMD51_DFP.1.2.139.atpack
-cd ..
+if [ ! -d Atmel.SAMD51_DFP.1.2.139 ]; then
+    curl -O http://packs.download.atmel.com/Atmel.SAMD51_DFP.1.2.139.atpack
+    unzip Atmel.SAMD51_DFP.1.2.139.atpack -d Atmel.SAMD51_DFP.1.2.139
+fi
 
-if [ ! -d ~/Downloads/arm-gnu-toolchain-14.2-*-arm-none-eabi ]; then
+if [ ! -d arm-gnu-toolchain-14.2.*-arm-none-eabi ]; then
     if uname -sm | grep 'Linux x86_64' ; then
-        curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz
-        tar Jxf arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz
+        TOOLCHAIN_TARBALL=arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz
     elif uname -sm | grep 'Linux aarch64' ; then
-        curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-aarch64-arm-none-eabi.tar.xz
-        tar Jxf arm-gnu-toolchain-14.2.rel1-aarch64-arm-none-eabi.tar.xz
+        TOOLCHAIN_TARBALL=arm-gnu-toolchain-14.2.rel1-aarch64-arm-none-eabi.tar.xz
     else
-        curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-darwin-arm64-arm-none-eabi.tar.xz
-        tar Jxf arm-gnu-toolchain-14.2.rel1-darwin-arm64-arm-none-eabi.tar.xz
+        TOOLCHAIN_TARBALL=arm-gnu-toolchain-14.2.rel1-darwin-arm64-arm-none-eabi.tar.xz
     fi
+
+    curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/"$TOOLCHAIN_TARBALL"
+    tar Jxf "$TOOLCHAIN_TARBALL"
 fi
 
-git clone --depth 1 https://github.com/shumatech/BOSSA
-cd BOSSA
-if c++ -v 2>&1 | grep clang ; then
-    make bossac CXXFLAGS="-Wno-error=unqualified-std-cast-call" 2>&1 | grep -v found >&2
-else
-    make bossac 2>&1 | grep -v found >&2
+if [ ! -d BOSSA ]; then
+    git clone --depth 1 https://github.com/shumatech/BOSSA
+    make -C BOSSA bossac CXXFLAGS="-Wno-unqualified-std-cast-call" 2>&1 | grep -v found >&2
 fi
-cd ..
