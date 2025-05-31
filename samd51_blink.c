173657e3985a10cb9da4f1b286dfcfcd6f9030c7
Richard Campbell <rlcamp@users.noreply.github.com>
2025-05-31 12:51:52 -0700

    use sevonpend

--- samd51_blink.c
+++ samd51_blink.c
@@ -47,9 +47,9 @@ static void timer_init(void) {
     TC3->COUNT16.CC[0].reg = 16383;
     while (TC3->COUNT16.SYNCBUSY.bit.COUNT);
 
-    /* fire an interrupt at lowest priority whenever counter equals that value */
+    /* fire an interrupt at lowest priority, but do NOT enable the ISR in the NVIC */
     TC3->COUNT16.INTENSET.bit.MC0 = 1;
-    NVIC_EnableIRQ(TC3_IRQn);
+    NVIC_DisableIRQ(TC3_IRQn);
 
     /* enable the timer */
     while (TC3->COUNT16.SYNCBUSY.reg);
@@ -57,21 +57,10 @@ static void timer_init(void) {
     while (TC3->COUNT16.SYNCBUSY.bit.ENABLE);
 }
 
-static unsigned wakes = 0;
-
-void TC3_Handler(void) {
-    /* clear flag so that interrupt doesn't re-fire */
-    TC3->COUNT16.INTFLAG.reg = (TC_INTFLAG_Type){ .bit.MC0 = 1 }.reg;
-
-    wakes++;
-
-    /* arm an321 page 22 fairly strongly suggests this is necessary prior to exception
-     return (or will be on future processors) when the first thing the processor wants
-     to do afterward depends on state changed in the handler */
-    __DSB();
-}
-
 int main(void) {
+    /* processor will wake up and return from wfe even if interrupt is disabled in nvic */
+    SCB->SCR |= SCB_SCR_SEVONPEND_Msk;
+
     PM->SLEEPCFG.bit.SLEEPMODE = PM_SLEEPCFG_SLEEPMODE_STANDBY_Val;
 
     led_init();
@@ -80,23 +69,18 @@ int main(void) {
     unsigned wakes_acknowledged = 0;
 
     while (1) {
-        if (*(volatile unsigned *)&wakes != wakes_acknowledged) {
-            wakes_acknowledged++;
+        /* sleep until next event (in this case the tc3 interrupt) */
+        while (!TC3->COUNT16.INTFLAG.bit.MC0)
+            __WFE();
 
-            /* toggle the led */
-            if (wakes_acknowledged % 2) led_on();
-            else led_off();
-        }
+        /* clear interrupt in peripheral, and clear pending irq in nvic */
+        TC3->COUNT16.INTFLAG.reg = (TC_INTFLAG_Type){ .bit.MC0 = 1 }.reg;
+        NVIC_ClearPendingIRQ(TC3_IRQn);
 
-        /* sleep until next event (in this case the tc3 interrupt), unless the event register
-         is already set, in which case it is cleared and no sleep occurs. note since no other
-         code calls WFE, there will be at least two passes through the main loop for every
-         interrupt, where on the final pass, no state is changed, ER is already cleared, and
-         the processor sleeps. in exchange for these redundant passes before sleeping, we get
-         a guarantee that we will never spuriously sleep when we should have stayed awake to
-         handle an already-pending interrupt, as is possible with WFI if not disabling
-         interrupts during sleep (which adds latency in handling them on wake) */
-        __DSB();
-        __WFE();
+        wakes_acknowledged++;
+
+        /* toggle the led */
+        if (wakes_acknowledged % 2) led_on();
+        else led_off();
     }
 }
